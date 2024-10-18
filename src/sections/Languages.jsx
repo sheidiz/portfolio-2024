10c7dbdd0ddfebee6cae287159c095bbe6acafae
Sheila Diz <dizsheila07@gmail.com>
2024-10-18 18:43:44 -0300

    feat: add and remove languages

--- src/sections/Languages.jsx
+++ src/sections/Languages.jsx
@@ -1,6 +1,5 @@
-import React from 'react'
+import React from 'react';
 import { useLanguage } from '../contexts/LanguageContext';
-import translations from '../translations/data';
 import htmlIcon from "./../assets/icons/html.png";
 import cssIcon from "./../assets/icons/css.png";
 import jsIcon from "./../assets/icons/javascript.png";
@@ -11,15 +10,15 @@ import tailwindIcon from "./../assets/icons/tailwind.png";
 import figmaIcon from "./../assets/icons/figma.png";
 import javaIcon from "./../assets/icons/java.png";
 import springIcon from "./../assets/icons/spring.png";
-import springjpaIcon from "./../assets/icons/springjpa.png";
 import gitIcon from "./../assets/icons/git.png";
 import mysqlIcon from "./../assets/icons/mysql.png";
+import postgresqlIcon from "./../assets/icons/postgresql.png";
+import postmanIcon from "./../assets/icons/postman.png";
 import junitIcon from "./../assets/icons/junit5.png";
-import mockitoIcon from "./../assets/icons/mockito.png";
+import swaggerIcon from "./../assets/icons/swagger.png";
 
 const Languages = () => {
   const { language } = useLanguage();
-  const t = translations[language];
 
   return (
     <section className='max-w-4xl mx-3 md:mt-3 md:mx-auto grid grid-cols-1 md:grid-cols-2 gap-y-3 md:gap-x-3'>
@@ -41,12 +40,12 @@ const Languages = () => {
         <ul className='mt-4 flex flex-wrap gap-4'>
           <li className='flex flex-col items-center text-sm font-medium'><img src={javaIcon} className='h-6' /> Java</li>
           <li className='flex flex-col items-center text-sm font-medium'><img src={springIcon} className='h-6' /> Spring</li>
-          <li className='flex flex-col items-center text-sm font-medium'><img src={springjpaIcon} className='h-6' /> Spring JPA</li>
           <li className='flex flex-col items-center text-sm font-medium'><img src={gitIcon} className='h-6' /> Git</li>
           <li className='flex flex-col items-center text-sm font-medium'><img src={mysqlIcon} className='h-6' /> MySQL</li>
+          <li className='flex flex-col items-center text-sm font-medium'><img src={postgresqlIcon} className='h-6' /> PostgreSQL</li>
+          <li className='flex flex-col items-center text-sm font-medium'><img src={swaggerIcon} className='h-6' /> Swagger</li>
+          <li className='flex flex-col items-center text-sm font-medium'><img src={postmanIcon} className='h-6' /> Postman</li>
           <li className='flex flex-col items-center text-sm font-medium'><img src={junitIcon} className='h-6' /> JUnit</li>
-          <li className='flex flex-col items-center text-sm font-medium'><img src={mockitoIcon} className='h-6' /> Mockito</li>
-          {/*<li className='flex flex-col items-center text-sm font-medium'><img src='/src/assets/icons/swagger.png' className='h-6' /> Swagger</li>*/}
         </ul>
       </div>
     </section>
